5acefeb28d1506028148714b0dae850f1213340a
Vyacheslav Voronenko <git@voronenko.info>
2019-03-20 22:07:58 +0100

    added external gitlab integration

--- deployment/docker_build.sh
+++ deployment/docker_build.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
 # do docker build --no-cache to drop cached parts
 
-set -xe
+WORKSPACE=$(
+  cd $(dirname "$0")
+  pwd
+)
 
+set -xe
 if [ -z $1 ]
 then
   TAG=":latest"
 else
   TAG=":$1"
 fi
-echo docker build --tag="softasap/sphinx-projectdocs$TAG" ../
-docker build --tag="softasap/sphinx-projectdocs$TAG" ../
+echo docker build --tag="softasap/sphinx-projectdocs$TAG" $WORKSPACE/../
+docker build --tag="softasap/sphinx-projectdocs$TAG" $WORKSPACE/../
 
 echo "Successfully built softasap/sphinx-projectdocs$TAG"
 
--- deployment/docker_push.sh
+++ deployment/docker_push.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+WORKSPACE=$(
+  cd $(dirname "$0")
+  pwd
+)
+
 set -xe
 
 if [ -z $1 ]
